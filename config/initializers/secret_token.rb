fd1a799b0aabe4182359abe2c29d04e444afb1fb
ZHAN Shengjie <kryss.col@gmail.com>
2011-03-04 17:49:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RAccount::Application.config.secret_token = '2e777fa0d93cab3a379a09d82990f419be24fd6f5fd2837a77a08e7b5c4bff5acbd147282da851466b7f34ace65ed2c93d1287d9f2ce5efc6d985e7a8f0f0758'
